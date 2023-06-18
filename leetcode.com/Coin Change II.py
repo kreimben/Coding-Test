bf23fe9dae69bbf70592ca4650972ed19c5b06af
Aksidion Kreimben <aksidion@kreimben.com>
2023-06-18 11:39:54 +0900

    .

--- leetcode.com/Coin Change II.py
+++ leetcode.com/Coin Change II.py
@@ -2,31 +2,52 @@
 https://leetcode.com/problems/coin-change-ii/
 Coin Change II
 """
+from functools import cache
 from typing import List
 
 
 class Solution:
     """
-    The very basic form of backtracking.
+    cached dfs.
     """
 
     def change(self, amount: int, coins: List[int]) -> int:
-        case = set()
-        arr = []
 
-        def backtracking():
-            nonlocal case, arr
-            if sum(arr) == amount:
-                case.add(tuple(sorted(arr)))
-                return
-            elif sum(arr) > amount:
-                return
+        @cache
+        def dfs(before: int, rest: int):
+            if rest == 0:
+                return 1  # means 1 case.
+            case = 0
+            for coin in coins:
+                if coin <= before and rest >= coin:
+                    case += dfs(coin, rest - coin)
+            return case
 
-            for denom in coins:
-                arr.append(denom)
-                backtracking()
-                arr.pop()
+        return dfs(max(coins), amount)
 
-        backtracking()
-
-        return len(case)
+# class Solution:
+#     """
+#     The very basic form of backtracking.
+#     backtracking is slow to pass all test cases.
+#     """
+#
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         case = set()
+#         arr = []
+#
+#         def backtracking():
+#             nonlocal case, arr
+#             if sum(arr) == amount:
+#                 case.add(tuple(sorted(arr)))
+#                 return
+#             elif sum(arr) > amount:
+#                 return
+#
+#             for denom in coins:
+#                 arr.append(denom)
+#                 backtracking()
+#                 arr.pop()
+#
+#         backtracking()
+#
+#         return len(case)
