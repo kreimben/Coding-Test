05311ce0aad195425de6f5f4ded64767a1187b1c
Aksidion Kreimben <aksidion@kreimben.com>
2023-06-21 01:06:51 +0900

    .

--- algoexpert.io/Best Seat.py
+++ algoexpert.io/Best Seat.py
@@ -5,5 +5,37 @@ Best Seat
 
 
 def bestSeat(seats):
-    # Write your code here.
-    return -1
+    # should return index of most appropriate seats
+    candidates = {}  # key is total current seat counts. value is index.
+    left, right = 1, 2
+    while right < len(seats):
+        while left >= right:
+            right += 1
+
+        if seats[left] != 0:
+            left += 1
+            continue
+
+        if seats[right] != 1:
+            right += 1
+            continue
+
+        if seats[left] == 0 and seats[right] == 1:
+            curr_count = right - left
+            if curr_count % 2 == 0:
+                candidates[curr_count] = min((right + left) // 2 - 1, candidates.get(curr_count, float('inf')))
+            else:
+                candidates[curr_count] = min((right + left) // 2, candidates.get(curr_count, float('inf')))
+
+        left = right
+        right += 1
+
+    if candidates.keys():
+        keys = sorted(candidates.keys(), reverse=True)
+        for key in keys:
+            return candidates[key]
+    else:
+        return -1
+
+
+assert bestSeat([1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1]) == 5
