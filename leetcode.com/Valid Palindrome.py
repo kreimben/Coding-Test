2b575240219a3c684482a848cc171793484e6881
Aksidion Kreimben <aksidion@kreimben.com>
2023-08-07 00:39:29 +0900

    .

--- leetcode.com/Valid Palindrome.py
+++ leetcode.com/Valid Palindrome.py
@@ -20,6 +20,22 @@ class Solution:
             return False
 
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()  # make sentence lower.
+        s = ''.join(filter(str.isalnum, s))  # remove non-alphanumeric chars in sentence.
+
+        left, right = 0, len(s) - 1
+
+        while left <= right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+
 s = Solution()
 
 print(s.isPalindrome("0P") == False)
