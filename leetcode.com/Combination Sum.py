7a9c8438d58616424aa0df8d6bafe9cd55e390df
Aksidion Kreimben <aksidion@kreimben.com>
2023-08-18 17:05:13 +0900

    .

--- leetcode.com/Combination Sum.py
+++ leetcode.com/Combination Sum.py
@@ -7,24 +7,49 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
+        # backtracking
+        temp = []
+        res = set()
 
-        def dfs(indicies, target):
-            nonlocal res
-            if target == 0:
-                res.append([candidates[index] for index in indicies])
+        def backtracking(next: int):
+            nonlocal temp, res
+            if sum(temp) > target:
                 return
-            elif target < 0:
+            elif sum(temp) == target:
+                res.add(tuple(sorted(temp)))
                 return
 
-            for i in range(indicies[-1], len(candidates)):
-                dfs(indicies + [i], target - candidates[i])
+            for i in range(next, len(candidates)):
+                temp.append(candidates[i])
+                backtracking(i)
+                temp.pop()
 
-        for i, num in enumerate(candidates):
-            dfs([i], target - num)
+        backtracking(0)
 
-        return res
+        return list(res)
+
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = []
+#
+#         def dfs(indicies, target):
+#             nonlocal res
+#             if target == 0:
+#                 res.append([candidates[index] for index in indicies])
+#                 return
+#             elif target < 0:
+#                 return
+#
+#             for i in range(indicies[-1], len(candidates)):
+#                 dfs(indicies + [i], target - candidates[i])
+#
+#         for i, num in enumerate(candidates):
+#             dfs([i], target - num)
+#
+#         return res
 
 
 s = Solution()
+assert s.combinationSum(candidates=[2, 3, 5], target=8) == [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
 assert s.combinationSum(candidates=[2, 3, 6, 7], target=7) == [[2, 2, 3], [7]]
