aeb427b1fbb0ae0d7116ca415a021fe905ddf8ab
Aksidion Kreimben <aksidion@kreimben.com>
2023-08-21 00:12:03 +0900

    .

--- leetcode.com/Implement Trie (Prefix Tree).py
+++ leetcode.com/Implement Trie (Prefix Tree).py
@@ -42,8 +42,44 @@ class Trie:
             curr = curr.children[o]
         return True
 
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+
+class Trie:
+
+    def __init__(self):
+        self.d = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.d
+        for ch in word:
+            if not curr.get(ch, None):
+                curr[ch] = {}
+            curr = curr[ch]
+        curr.update({'*': True})
+        return
+
+    def search(self, word: str) -> bool:
+        curr = self.d
+        for ch in word:
+            if not curr.get(ch, None):
+                return False
+            curr = curr[ch]
+        return curr.get('*', None)
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.d
+        for ch in prefix:
+            if not curr.get(ch, None):
+                return False
+            curr = curr[ch]
+        return True
+
+# s = Trie()
+# s.insert('apple')
+# assert s.search('apple') == True
