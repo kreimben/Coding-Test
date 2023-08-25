ab6207afd4772492be7350732c15b1e725333821
Aksidion Kreimben <aksidion@kreimben.com>
2023-08-25 16:10:51 +0900

    .

--- leetcode.com/Coin Change.py
+++ leetcode.com/Coin Change.py
@@ -2,7 +2,31 @@
 https://leetcode.com/problems/coin-change/description/
 Coin Change
 """
+import math
+from functools import lru_cache
 from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort(reverse=True)
+
+        @lru_cache(maxsize=None)
+        def dfs(rest: int) -> int:
+            if rest == 0:
+                return 0
+            elif rest < 0:
+                return math.inf
+
+            temp = math.inf
+            for coin in coins:
+                res = dfs(rest - coin) + 1
+                temp = min(temp, res)
+
+            return temp
+
+        res = dfs(amount)
+        return res if res != math.inf else -1
 
 
 class Solution:
@@ -17,4 +41,5 @@ class Solution:
 
 
 s = Solution()
+# assert s.coinChange([411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422], 9864) == 24
 assert s.coinChange([1, 2, 5], 11) == 3
