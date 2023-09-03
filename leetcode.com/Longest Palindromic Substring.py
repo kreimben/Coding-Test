ebbcc75059f92650446749f6dd436a57d9e74032
Aksidion Kreimben <aksidion@kreimben.com>
2023-09-04 00:09:21 +0900

    .

--- leetcode.com/Longest Palindromic Substring.py
+++ leetcode.com/Longest Palindromic Substring.py
@@ -2,13 +2,57 @@
 https://leetcode.com/problems/longest-palindromic-substring/
 Longest Palindromic Substring
 """
+from functools import lru_cache
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = ''
+
+        for i in range(len(s)):
+            # Odd case
+            left, right = i, i
+            while 0 <= left <= right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(longest):
+                    longest = s[left:right + 1]
+                left -= 1
+                right += 1
+
+            # Even case
+            left, right = i, i + 1
+            while 0 <= left <= right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > len(longest):
+                    longest = s[left:right + 1]
+                left -= 1
+                right += 1
+
+        return longest
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = ''
+
+        @lru_cache(maxsize=None)
+        def calculate(start, end):
+            nonlocal longest
+            if start < end and end <= len(s):
+                block = s[start:end]
+                if block == block[::-1] and len(longest) < len(block):
+                    longest = block
+                    calculate(end, end + 1)
+                calculate(start, end + 1)
+                calculate(start + 1, end + 1)
+
+        calculate(0, 1)
+        return longest
 
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         dp = ['' for _ in range(len(s) + 1)]
         # None b ba bab baba babad (dp table)
-        #  ''  b  b bab bab   bab  (lognest palindrom)
+        #  ''  b  b bab bab   bab  (longest palindrome)
         for i in range(1, len(dp)):
             def find(left: int, right: int):
                 nonlocal s, i
@@ -19,7 +63,7 @@ class Solution:
                         break
                     b = s[left: right + 1]
                     if b == b[::-1]:
-                        # Don't need to find furthur.
+                        # Don't need to find further.
                         dp[i] = b
                         break
                     find(left + 1, right)
