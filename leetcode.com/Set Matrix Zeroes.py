829571599449df2b20e294b38d76dc018e10fe30
Aksidion Kreimben <aksidion@kreimben.com>
2023-09-06 00:02:53 +0900

    .

--- leetcode.com/Set Matrix Zeroes.py
+++ leetcode.com/Set Matrix Zeroes.py
@@ -7,6 +7,25 @@ from typing import List
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        stack = []
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    stack.append((i, j))
+
+        while stack:
+            i, j = stack.pop()
+            # e.i) 0, 0 => x, 0 and 0, y
+            for x in range(len(matrix[0])):
+                matrix[i][x] = 0
+
+            for y in range(len(matrix)):
+                matrix[y][j] = 0
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         # Must find where is 0
         x, y = [], []
 
