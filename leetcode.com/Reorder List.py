f3ec96d289cff04985a453015451d021f5811202
Aksidion Kreimben <aksidion@kreimben.com>
2023-09-13 09:08:21 +0900

    .

--- leetcode.com/Reorder List.py
+++ leetcode.com/Reorder List.py
@@ -2,6 +2,7 @@
 https://leetcode.com/problems/reorder-list/
 Reorder List
 """
+from typing import Optional
 
 
 # Definition for singly-linked list.
@@ -11,60 +12,42 @@ class ListNode:
         self.next = next
 
 
-def get_inner_values(node: ListNode) -> [int]:
-    curr = node
-    results = []
-    while curr:
-        results.append(curr.val)
-        curr = curr.next
-
-    return results
-
-
-def deploy_values(node: ListNode, values: [int]):
-    curr = node
-    index = 0
-    while curr:
-        curr.val = values[index]
-        index += 1
-        curr = curr.next
-
-
 class Solution:
-    def reorderList(self, head: ListNode) -> None:
-        values: [int] = get_inner_values(head)
-        mid_index = len(values) // 2
-        index = 1  # The number being inserted should be inserted right next first element of originals.
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # get middle node of list nodes.
+        mid, end = head, head
+        while end and end.next:
+            mid = mid.next
+            end = end.next.next
+        if end:
+            mid = mid.next
 
-        while index <= mid_index:
-            last = values.pop()
-            values.insert(index, last)
-            index += 2
-            mid_index += 1
+        # cut off head after mid
+        curr = head
+        while curr.next != mid:
+            curr = curr.next
+        curr.next = None
 
-        deploy_values(head, values)
+        def reverse(node: ListNode) -> ListNode | None:
+            prev: ListNode | None = None
+            while node:
+                next = node.next
+                node.next = prev
+                prev = node
+                node = next
+            return prev
 
+        mid = reverse(mid)
 
-s = Solution()
-input_node = ListNode(1,
-                      ListNode(2,
-                               ListNode(3,
-                                        ListNode(4,
-                                                 ListNode(5)
-                                                 )
-                                        )
-                               )
-                      )
-s.reorderList(
-    input_node
-)
-expected = ListNode(1,
-                    ListNode(5,
-                             ListNode(2,
-                                      ListNode(4,
-                                               ListNode(3)
-                                               )
-                                      )
-                             )
-                    )
-assert input_node == expected
+        # assemble
+        curr = head
+        while curr and mid:
+            next = curr.next
+            midnext = mid.next
+            curr.next = mid
+            curr.next.next = next
+
+            curr = curr.next.next
+            mid = midnext
+
+        return curr
