054a7e5c2fc1a55a4fd897898cc2c2b84f310e91
Aksidion Kreimben <aksidion@kreimben.com>
2023-09-24 19:04:01 +0900

    .

--- leetcode.com/Product of Array Except Self.py
+++ leetcode.com/Product of Array Except Self.py
@@ -2,37 +2,57 @@
 https://leetcode.com/problems/product-of-array-except-self/
 Product of Array Except Self
 """
-from collections import defaultdict
 
 
 class Solution:
-    prefix = defaultdict(int)
-    postfix = defaultdict(int)
-
-    def count_prefix(self, index: int, last_num: int):
-        self.prefix[index] = self.prefix[index - 1] * last_num
-
-    def count_postfix(self, index: int, first_num: int):
-        self.postfix[index] = self.postfix[index + 1] * first_num
-
-    def calculate(self, index: int) -> int:
-        result = self.prefix[index - 1] * self.postfix[index + 1]
-        return result
-
     def productExceptSelf(self, nums: [int]) -> [int]:
-        results = []
-        self.prefix[-1] = 1
-        self.postfix[len(nums)] = 1
+        N = len(nums)
+        res = [1] * N
 
-        for i in range(len(nums)):
-            self.count_prefix(i, nums[i])
-            self.count_postfix(len(nums) - 1 - i, nums[len(nums) - 1 - i])
+        prefix = 1
+        for i in range(N):
+            res[i] = prefix
+            prefix *= nums[i]
 
-        for i in range(len(nums)):
-            r = self.calculate(i)
-            results.append(r)
+        suffix = 1
+        for i in range(N - 1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
 
-        return results
+        return res
+
+
+# from collections import defaultdict
+#
+#
+# class Solution:
+#     prefix = defaultdict(int)
+#     postfix = defaultdict(int)
+#
+#     def count_prefix(self, index: int, last_num: int):
+#         self.prefix[index] = self.prefix[index - 1] * last_num
+#
+#     def count_postfix(self, index: int, first_num: int):
+#         self.postfix[index] = self.postfix[index + 1] * first_num
+#
+#     def calculate(self, index: int) -> int:
+#         result = self.prefix[index - 1] * self.postfix[index + 1]
+#         return result
+#
+#     def productExceptSelf(self, nums: [int]) -> [int]:
+#         results = []
+#         self.prefix[-1] = 1
+#         self.postfix[len(nums)] = 1
+#
+#         for i in range(len(nums)):
+#             self.count_prefix(i, nums[i])
+#             self.count_postfix(len(nums) - 1 - i, nums[len(nums) - 1 - i])
+#
+#         for i in range(len(nums)):
+#             r = self.calculate(i)
+#             results.append(r)
+#
+#         return results
 
 
 s = Solution()
