3adeb0cb81219f9e060be2cf948a3abce3629805
Aksidion Kreimben <aksidion@kreimben.com>
2023-10-06 23:31:08 +0900

    .

--- leetcode.com/Missing Number.py
+++ leetcode.com/Missing Number.py
@@ -12,3 +12,24 @@ class Solution:
         for i in range(len(nums)):
             res += i - nums[i]
         return res
+
+
+class Solution:
+    """
+    it's not O(1) space complexity
+    """
+
+    def missingNumber(self, nums: List[int]) -> int:
+        maxnum = max(len(nums), max(nums))
+        check = [0] * (maxnum + 1)
+        for i in range(maxnum + 1):
+            if i in nums:
+                check[i] = 1
+        return check.index(0)
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        N = max(max(nums), len(nums))
+        total = sum([i for i in range(N + 1)])
+        return total - sum(nums)
